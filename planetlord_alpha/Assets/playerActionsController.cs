d3f5d7b8d1b267bca5e2ab3b33dbd140685c2828
Ray Grice <interwebsninja@bitbucket.org>
2014-09-11 22:52:13 -0700

    Initial Upload

---
+++ planetlord_alpha/Assets/playerActionsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playerActionsController : MonoBehaviour 
+{
+
+	void Update()
+	{
+		inputListener();
+	}
+
+	//Handles all input from the player.
+	void inputListener();
+	{
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+		float verticalAxis = Input.GetAxisRaw("Vertical");
+
+		engineControl(horizontalAxis, verticalAxis);
+		if (Input.GetButtonDown("Jump"))
+		{
+			stopMovement();
+		}
+		if (Input.GetButton("Weapon1"))
+		{
+			weaponSelect(0);
+		}
+		else if (Input.GetButton("Weapon2"))
+		{
+			weaponSelect(1);
+		}
+		if(Input.GetButton("FireWeapon"))
+		{
+			fireProjectile();
+		}
+	}
+}
